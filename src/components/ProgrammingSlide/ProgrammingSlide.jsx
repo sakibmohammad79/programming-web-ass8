ca51c53b723a9c3417604b8ef6d282cd9d04d38c
Mohammad Sakib <mohammadsakib7679@gmail.com>
2023-03-31 01:15:35 +0600

    data display in programming slide components

--- src/components/ProgrammingSlide/ProgrammingSlide.jsx
+++ src/components/ProgrammingSlide/ProgrammingSlide.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
+import './ProgrammingSlide.css';
 
 const ProgrammingSlide = (props) => {
-    const {name, id, title, time, date} = props.data;
+    const {slide_picture, outhor_picture, name, id, title, time, date} = props.data;
     return (
-        <div>
-            <p>name: {name}</p>
+        <div className='data-container'>
+                <div>
+                <img className='cover-img' src={slide_picture} alt="" />
+                </div>
+                <div className='slide-info pt-3 ps-1 pe-3 pb-3'>
+                    <div className='d-flex justify-content-between'>
+                        <div className='d-flex align-items-center'>
+                            <img className='author-img' src={outhor_picture} alt="" />
+                            <div className='ms-3'>
+                            <p>{name}</p>
+                            <p>{date}</p>
+                            </div>
+                        </div>
+                        <div>
+                            <p>{time}</p>
+                        </div>
+                    </div>
+                    <p>{title}</p>
+                    <p>#beginners  #programming</p>
+                    <a href="#">Mark as read</a>
+                </div>
         </div>
     );
 };
